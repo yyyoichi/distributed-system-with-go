94c7a7851d90b2c089051acb78ee832335c34ed3
yyyoichi <origamiichiro@gmail.com>
2023-11-10 12:22:06 +0000

    bug size should be added

--- internal/log/store.go
+++ internal/log/store.go
@@ -39,16 +39,17 @@ func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	pos = s.size
+	// bufにbigendianで64bitで表現したpの長さを格納する
 	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
 		return 0, 0, err
 	}
-
+	// buf にpを書き込む
 	w, err := s.buf.Write(p)
 	if err != nil {
 		return 0, 0, err
 	}
 	w += lenWidth
-	s.size = uint64(w)
+	s.size += uint64(w)
 	return uint64(w), pos, nil
 }
 
@@ -58,10 +59,12 @@ func (s *store) Read(pos uint64) ([]byte, error) {
 	if err := s.buf.Flush(); err != nil {
 		return nil, err
 	}
+	// 64bitを読込、データの長さsizeを取得する
 	size := make([]byte, lenWidth)
 	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
 		return nil, err
 	}
+	// データの長さsize分のデータを読み込む
 	b := make([]byte, enc.Uint64(size))
 	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
 		return nil, err
