ec155426811d2b439217ae2e62c4ab81ce07a4d6
yyyoichi <origamiichiro@gmail.com>
2023-11-12 09:49:34 +0000

    feat add segment

---
+++ internal/log/segment_test.go
@@ -0,0 +1,65 @@
+package log
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	api "github.com/yyyoichi/proglog/api/v1"
+	"github.com/yyyoichi/proglog/pkg/proto"
+)
+
+func TestSegment(t *testing.T) {
+	dir, _ := os.MkdirTemp("", "segment-test")
+	defer os.RemoveAll(dir)
+
+	want := &api.Record{Value: []byte("hello world")}
+
+	c := Config{}
+
+	// test case.1
+	// indexfileが最大に達する
+	c.Segment.MaxStoreBytes = 1024
+	c.Segment.MaxIndexBytes = entwidth * 3
+
+	s, err := newSegment(dir, 16, c)
+	require.NoError(t, err)
+	require.Equal(t, uint64(16), s.nextOffset)
+	require.False(t, s.IsMaxed())
+
+	for i := uint64(0); i < 3; i++ {
+		off, err := s.Append(want)
+		require.NoError(t, err)
+		require.Equal(t, 16+i, off)
+
+		got, err := s.Read(off)
+		require.NoError(t, err)
+		require.Equal(t, want.Value, got.Value)
+	}
+
+	_, err = s.Append(want)
+	require.Equal(t, io.EOF, err)
+
+	require.True(t, s.IsMaxed())
+	require.NoError(t, s.Close())
+
+	// test case.2
+	// store fileが最大に達する
+	p, _ := proto.Marshal(want)
+	c.Segment.MaxStoreBytes = uint64(len(p)+lenWidth) * 4
+	c.Segment.MaxIndexBytes = 1024
+
+	s, err = newSegment(dir, 16, c)
+	require.NoError(t, err)
+
+	require.True(t, s.IsMaxed())
+
+	require.NoError(t, s.Remove())
+
+	s, err = newSegment(dir, 16, c)
+	require.NoError(t, err)
+	require.False(t, s.IsMaxed())
+	require.NoError(t, s.Close())
+
+}
