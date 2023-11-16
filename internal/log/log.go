ab8013d8328927d9c1a66515880f1fe358cbf8d3
yyyoichi <origamiichiro@gmail.com>
2023-11-16 14:38:17 +0000

    feat add log setup

---
+++ internal/log/log.go
@@ -0,0 +1,50 @@
+package log
+
+import (
+	"os"
+	"path"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type Log struct {
+	mu sync.RWMutex
+
+	Dir    string
+	Config Config
+
+	activeSegment *segment
+	segments      []*segment
+}
+
+func NewLog(dir string, c Config) (*Log, error) {
+	if c.Segment.MaxStoreBytes == 0 {
+		c.Segment.MaxStoreBytes = 1024
+	}
+	if c.Segment.MaxIndexBytes == 0 {
+		c.Segment.MaxIndexBytes = 1024
+	}
+	l := &Log{
+		Dir:    dir,
+		Config: c,
+	}
+	return l, l.setup()
+}
+
+func (l *Log) setup() error {
+	files, err := os.ReadDir(l.Dir)
+	if err != nil {
+		return err
+	}
+	var baseOffset []uint64
+	for _, file := range files {
+		offStr := strings.TrimSuffix(
+			file.Name(),
+			path.Ext(file.Name()),
+		)
+		off, _ := strconv.ParseUint(offStr, 10, 0)
+		baseOffset = append(baseOffset, off)
+	}
+	return nil
+}
